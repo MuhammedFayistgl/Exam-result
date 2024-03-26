001545b7dde597c3e27e95f98b10d4152c4f1a15
Muhammad Fayis Thangal <77223454+MuhammedFayistgl@users.noreply.github.com>
2024-03-25 19:50:18 -0500

    update

--- src/components/utils/AxiosInstence.ts
+++ src/components/utils/AxiosInstence.ts
@@ -1,13 +1,14 @@
 import axios from "axios";
+// import env from "react-dotenv";
 
+// const url = env.NODE_ENV === "production" ? "https://exam-result.onrender.com" : "http://localhost:8000/";
 export const AxiosInstance = axios.create({
-    baseURL: "https://exam-result.onrender.com",
-    // baseURL: "http://localhost:8000/",
-    withCredentials: true,
-    headers: {
-        "Accept": "application/json",
-        "Content-Type": "application/json",
-        // "content-type": "multipart/form-data",
-        // 'Authorization': `Bearer ${document.cookie}`,
-    },
+	baseURL:"https://exam-result.onrender.com",
+	withCredentials: true,
+	headers: {
+		Accept: "application/json",
+		"Content-Type": "application/json",
+		// "content-type": "multipart/form-data",
+		// 'Authorization': `Bearer ${document.cookie}`,
+	},
 });
